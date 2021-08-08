8c5862ffd7c9d8a496bbd8c6ddd298ece12907ae
Harvey-Mitchell <tech.mitchh@gmail.com>
2021-08-08 12:33:57 -0500

    Created beginning files
    gitignore, Query 1, README, Data folder, Slide folder, Query Folder.

---
+++ Queries/Question1.sql
@@ -0,0 +1,3 @@
+/*Question 1:
+We want to understand more about the movies that families are watching. The following categories are considered family 
+movies: Animation, Children, Classics, Comedy, Family and Music.*/
