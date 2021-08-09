8187b3cc4cde103be80fabf8d17b2a846f756b18
Harvey-Mitchell <tech.mitchh@gmail.com>
2021-08-08 19:05:02 -0500

    Sql query has upated rental count
    added test sql to gitignore

--- Queries/Question1.sql
+++ Queries/Question1.sql
@@ -1,3 +1,8 @@
-/*Question 1:
-We want to understand more about the movies that families are watching. The following categories are considered family 
-movies: Animation, Children, Classics, Comedy, Family and Music.*/
+SELECT f.title, i.inventory_id, r.inventory_id, COUNT (f.title)
+  FROM film f
+  LEFT JOIN inventory i
+    ON f.film_id = i.inventory_id
+  LEFT JOIN rental r
+    ON i.inventory_id = r.inventory_id
+GROUP BY 1,2,3
+ORDER BY f.title;
