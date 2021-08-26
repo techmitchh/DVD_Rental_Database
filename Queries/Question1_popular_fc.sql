d874cb70ea1ffee59abbd82fcfed90dd9b6fdd9a
Harvey-Mitchell <tech.mitchh@gmail.com>
2021-08-25 20:33:12 -0500

    Corrected sql
    - Improved and accurate rental movie count.

--- Queries/Question1_popular_fc.sql
+++ Queries/Question1_popular_fc.sql
@@ -2,24 +2,23 @@
 What film categories are most popular with families? The following categories are considered family 
 movies: Animation, Children, Classics, Comedy, Family and Music.*/
 
-WITH films as (
-	SELECT f.title AS film_name, 
-		   c.name AS category,
-		   r.rental_ID AS rental
-	  FROM film AS f
-      JOIN film_category as fc
-	 USING (film_id)
-	  JOIN category AS c
-	 USING (category_id)
-	  JOIN inventory AS i
-		ON f.film_id = i.inventory_id
-	  JOIN rental AS r
-		ON i.inventory_id = r.inventory_id
-	 WHERE c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
-)
+/*Question 1:
+What film categories are most popular with families? The following categories are considered family 
+movies: Animation, Children, Classics, Comedy, Family and Music.*/
 
-SELECT film_name, category,
- COUNT (*) as rental_count
-  FROM films
-GROUP BY 1,2
-ORDER BY 2;
+
+SELECT 	f.title,
+		c.name AS category,
+		COUNT(*) AS rental_count
+  FROM 	film AS f
+      	JOIN film_category AS fc
+	 	USING (film_id)
+	  	JOIN category AS c
+	 	USING (category_id)
+	  	JOIN inventory AS i
+		ON f.film_id = i.film_id
+	  	JOIN rental AS r
+		ON i.inventory_id = r.inventory_id
+ WHERE  c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+ GROUP BY 1,2
+ ORDER BY 3 DESC;
